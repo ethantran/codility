c56a5de68b5a33e9ed64db5050daa06c3aae0a28
Ethan Tran <ethan.tran@7-11.com>
2019-07-24 17:23:05 -0500

    CountDiv - Optimize performance

--- CountDiv.js
+++ CountDiv.js
@@ -3,30 +3,37 @@
 
 function solution(A, B, K) {
   // write your code in JavaScript (Node.js 8.9.4)
-  let i = A;
   let j = B;
   let count = 0;
-  while (i <= j) {
-    let skipj = i === j;
-    let result = i % K;
-    if (result === 0) {
-      count++;
-      i += K;
-    } else if (i < K) {
-      i += Math.abs(i - K);
+  while (j > A) {
+    let result;
+    let div = j / K;
+    if (Number.isInteger(div)) {
+      count += div;
+      j -= div * K;
     } else {
-      i += result;
+      result = j % K;
+      if (result === 0) {
+        count++;
+        j -= K;
+      } else if (j < K) {
+        j -= Math.abs(j - K);
+      } else {
+        j -= result;
+      }
     }
-    if (skipj) continue;
-    result = j % K;
-    if (result === 0) {
+  }
+  if (A === B) {
+    if (A === 0 || A % K === 0) {
       count++;
-      j -= K;
-    } else if (j < K) {
-      j -= Math.abs(j - K);
-    } else {
-      j -= result;
     }
+    return count;
+  }
+  if (A > 0 && B > 0) {
+    return count - solution(0, A - 1, K) + 1;
+  }
+  if (A === 0) {
+    return count + 1;
   }
   return count;
 }
@@ -40,3 +47,13 @@ a = solution(11, 345, 17);
 a; // 20
 a = solution(0, 0, 11);
 a; // 1
+a = solution(1, 1, 11);
+a; // 0
+a = solution(10, 10, 5);
+a; // 1
+a = solution(10, 10, 7);
+a; // 0
+a = solution(10, 10, 20);
+a; // 0
+a = solution(0, 14, 2);
+a; // 8
